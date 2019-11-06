63c36b57a9135cd4ac68ea0baeb70a8156110376
Memo <memito_095@hotmail.com>
2019-11-05 19:42:50 -0600

    Implemented pinch to zoom in file details.

--- app/src/main/java/com/guillermonegrete/gallery/FileDetailsFragment.kt
+++ app/src/main/java/com/guillermonegrete/gallery/FileDetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.guillermonegrete.gallery
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -7,14 +8,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 
 class FileDetailsFragment : Fragment() {
 
+    private lateinit var bottomSheet: LinearLayout
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,6 +35,13 @@ class FileDetailsFragment : Fragment() {
 
         val linkButton: ImageButton = root.findViewById(R.id.open_link_button)
         linkButton.setOnClickListener { openLink(file) }
+
+        bottomSheet = root.findViewById(R.id.bottom_layout)
+        // We need to return true so the bottom sheet handles the touch events.
+        // We only do this because otherwise PhotoView consumes the events.
+        bottomSheet.setOnTouchListener { _, _ ->
+            return@setOnTouchListener true
+        }
 
         return root
     }
