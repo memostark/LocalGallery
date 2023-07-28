ac7a320effa7cc6dc41e4b90b4847caa038531d5
Memo <memito_095@hotmail.com>
2023-07-27 23:55:13 -0600

    Enabled hilt for instrumentation tests.

---
+++ app/src/androidTest/java/com/guillermonegrete/gallery/CustomTestRunner.kt
@@ -0,0 +1,12 @@
+package com.guillermonegrete.gallery
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+
+class CustomTestRunner: AndroidJUnitRunner() {
+
+    override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication_Application::class.java.name, context)
+    }
+}
