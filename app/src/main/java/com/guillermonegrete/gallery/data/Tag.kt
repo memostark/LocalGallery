9f0c47c418733171e0a94fcae91caf0f586e90ad
Memo <memito_095@hotmail.com>
2023-08-16 23:05:26 -0600

    Displays the tag count in the sorting dialog.

--- app/src/main/java/com/guillermonegrete/gallery/data/Tag.kt
+++ app/src/main/java/com/guillermonegrete/gallery/data/Tag.kt
@@ -5,4 +5,23 @@ import kotlinx.parcelize.Parcelize
 import java.util.*
 
 @Parcelize
-data class Tag(val name: String, val creationDate: Date, val id: Long): Parcelable
+data class Tag(
+    val name: String,
+    val creationDate: Date,
+    val id: Long,
+    /**
+     * The amount of files in the folder that have this tag applied.
+     */
+    val count: Long = 0,
+): Parcelable
+
+
+data class TagCount(
+    val name: String,
+    val creationDate: Date,
+    /**
+     * The amount of files in the folder that have this tag applied.
+     */
+    val count: Long,
+    val id: Long,
+)
