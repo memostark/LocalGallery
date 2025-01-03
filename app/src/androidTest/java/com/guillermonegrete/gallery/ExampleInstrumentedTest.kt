e21ed096e85d809c4a6920fc2ddff5edd23a0fff
Memo <memito_095@hotmail.com>
2025-01-02 23:56:40 -0600

    Added suggestions from the build analyzer. (#168)
    Disabled jetifier and update the dex dependency to AndroidX.
    Enabled Gradle configuration cache.
    App is no longer uninstalled after running Android tests.
    Removed use of deprecated instrumentation test classes.
    Updated KSP.
    Removed navigation args directory configuration that was creating problems with duplicated generated files.

--- app/src/androidTest/java/com/guillermonegrete/gallery/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/guillermonegrete/gallery/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.guillermonegrete.gallery
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,10 +15,11 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.guillermonegrete.gallery", appContext.packageName)
     }
 }
