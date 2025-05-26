b82c425001c0b5672998005a6d5f2d703f16d618
Memo <memito_095@hotmail.com>
2025-05-25 23:59:30 -0600

    Added different main layout for different device width size.

--- app/src/main/java/com/guillermonegrete/gallery/folders/MainActivity.kt
+++ app/src/main/java/com/guillermonegrete/gallery/folders/MainActivity.kt
@@ -2,45 +2,59 @@ package com.guillermonegrete.gallery.folders
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.guillermonegrete.gallery.R
+import com.guillermonegrete.gallery.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main){
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge(navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT))
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.main_bottom_nav)
-        bottomNavigationView.setupWithNavController(navController)
+        val navigationView = binding.mainNavView
+
+        when(navigationView) {
+            is NavigationBarView -> navigationView.setupWithNavController(navController)
+            is NavigationView -> NavigationUI.setupWithNavController(navigationView, navController)
+        }
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            bottomNavigationView.isGone = when (destination.id) {
+            navigationView.isGone = when (destination.id) {
                 R.id.file_details_dest, R.id.addTagFragment -> true
                 // Keep the bar hidden when navigating to the files list from details
                 R.id.files_fragment_dest -> navController.previousBackStackEntry?.destination?.id == R.id.file_details_dest
                 else -> false
             }
 
-            if(destination.id == R.id.files_fragment_dest)
-                bottomNavigationView.menu.findItem(R.id.folders_fragment_dest).isChecked = true
+            if(destination.id == R.id.files_fragment_dest) {
+                when(navigationView) {
+                    is NavigationBarView -> navigationView.menu.findItem(R.id.folders_fragment_dest).isChecked = true
+                    is NavigationView -> navigationView.menu.findItem(R.id.folders_fragment_dest).isChecked = true
+                }
+            }
         }
     }
 
     fun showSnackBar(message: String) {
-        val navBar = findViewById<View>(R.id.main_bottom_nav)
+        val navBar = binding.mainNavView
         val snackBar = Snackbar.make(navBar, message, Snackbar.LENGTH_SHORT)
         snackBar.setAnchorView(navBar)
         snackBar.show()
